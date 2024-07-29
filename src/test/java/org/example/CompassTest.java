4e297c1ef16d20e28b37d6e7be5ee1a30dff5efe
alex-sumy-ua <plachkovskyy@yahoo.com>
2024-07-29 12:03:48 +0100

    TDD: Step 6 completed

--- src/test/java/org/example/CompassTest.java
+++ src/test/java/org/example/CompassTest.java
@@ -11,8 +11,20 @@ class CompassTest {
     @DisplayName("First test: Step 3")
     public void rotateTest() {
         Compass compass = new Compass();
-        assertEquals(null, compass.rotate(null, null));
-        assertEquals(Compass.Point.NORTH, compass.rotate(Compass.Point.EAST, Compass.Direction.RIGHT));
+        assertAll("Grouped Assertions of RIGHT Direction",
+                () -> assertEquals(null, compass.rotate(null, null)),
+
+                () -> assertEquals(Compass.Point.NORTH, compass.rotate(Compass.Point.EAST, Compass.Direction.RIGHT)),
+                () -> assertEquals(Compass.Point.WEST, compass.rotate(Compass.Point.NORTH, Compass.Direction.RIGHT)),
+                () -> assertEquals(Compass.Point.EAST, compass.rotate(Compass.Point.SOUTH, Compass.Direction.RIGHT)),
+                () -> assertEquals(Compass.Point.SOUTH, compass.rotate(Compass.Point.WEST, Compass.Direction.RIGHT)),
+
+                () -> assertEquals(Compass.Point.SOUTH, compass.rotate(Compass.Point.EAST, Compass.Direction.LEFT)),
+                () -> assertEquals(Compass.Point.EAST, compass.rotate(Compass.Point.NORTH, Compass.Direction.LEFT)),
+                () -> assertEquals(Compass.Point.WEST, compass.rotate(Compass.Point.SOUTH, Compass.Direction.LEFT)),
+                () -> assertEquals(Compass.Point.NORTH, compass.rotate(Compass.Point.WEST, Compass.Direction.LEFT)));
+
+
     }
 
 }
